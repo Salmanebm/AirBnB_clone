7b8df36429997ec6fa434188b562c2592e5ee236
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-06 21:56:50 +0200

    Adding save() method to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,25 @@ import uuid
 
 class BaseModel:
     """ Represents Base Model class. """
-    def __int__(self):
+    def __init__(self):
         """
         Instance initialization
         """
         self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Customized string representaion of the class
+        :return: None
+        """
+        print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        :return: None
+        """
+        self.updated_at = datetime.now()
