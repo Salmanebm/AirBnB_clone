278eaf78b6d8137c873455cbe48ef6bba2c3b602
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-07 11:45:03 +0200

    Adding **Kwargs to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,23 @@ import uuid
 
 class BaseModel:
     """ Represents Base Model class. """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Instance initialization
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
-        Customized string representaion of the class
+        Customized string representation of the class
         :return: None
         """
         print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
@@ -39,3 +45,24 @@ class BaseModel:
         self.__dict__["created_at"] = str(self.created_at.isoformat())
         self.__dict__["updated_at"] = str(self.updated_at.isoformat())
         return self.__dict__
+
+
+""" *********** TEST ************* """
+obj1 = BaseModel(name="Alice", age=25, __class_="Adel")
+print("Attributes from kwargs:")
+print(obj1.__dict__)  # Print the object's attributes
+
+# Create an instance without any kwargs (default attributes will be used)
+obj2 = BaseModel()
+print("\nDefault attributes:")
+print(obj2.__dict__)  # Print the object's attributes
+
+# Access specific attributes
+print(f"\nName: {obj1.name}")
+print(f"Age: {obj1.age}")
+
+# Access default attributes
+print(f"\nID: {obj2.id}")
+print(f"Created At: {obj2.created_at}")
+print(f"Updated At: {obj2.updated_at}")
+
