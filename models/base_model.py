9581c2b6e694b2d50b0e304072a16b9a175414a0
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-11-08 14:23:27 +0100

    Adding Base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-"""
-This module has the Base Model class
-"""
-from datetime import datetime
-import uuid
 
+import uuid
+from datetime import datetime
 
 class BaseModel:
-    """ Represents Base Model class. """
-    def __int__(self):
-        """
-        Instance initialization
-        """
-        self.id = uuid.uuid4()
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize the BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
