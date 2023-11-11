3eea74c855647ee08e90f637146231b4f55f6050
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-11 08:10:19 +0200

    Adding the command interpreter class

---
+++ console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module has the entry point to the command interpreter
+"""
+import cmd
+
+
+class HBBNCommand(cmd.Cmd):
+    """
+    Represents command interpreter class
+    """
+
+
+if __name__ == "__main__"
+    HBBNCommand().cmdloop()
