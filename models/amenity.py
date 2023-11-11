f9d5a6adfd094ec352565f4effebb24c3e646b64
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-11 07:04:29 +0200

    Adding amenity and city classes

--- models/amenity.py
+++ models/amenity.py
@@ -5,3 +5,8 @@ This module has the Amenity class
 from models.base_model import BaseModel
 
 
+class Amenity(BaseModel):
+    """
+    Represents Amenity class
+    """
+    name = ""
--- models/city.py
+++ models/city.py
@@ -5,4 +5,9 @@ This module has the City class
 from models.base_model import BaseModel
 
 
-
+class City(BaseModel):
+    """
+    Represents City class
+    """
+    state_id = ""
+    name = ""
