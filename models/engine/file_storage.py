9f901354ca562f4ec01931157984ac2719bf59c9
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-11 07:13:19 +0200

    Updating File Storage file to cope with new models.files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@ This module has FileStorage class
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
