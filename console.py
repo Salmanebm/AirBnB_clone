17f0143632653f5dc3a7a756b696baf5a35a41ab
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-11 08:41:08 +0200

    Finished Task 6

--- console.py
+++ console.py
@@ -9,7 +9,33 @@ class HBBNCommand(cmd.Cmd):
     """
     Represents command interpreter class
     """
+    prompt = '(hbnb) '
+
+    def do_help(self, arg):
+        """
+        Represents the help command
+        """
+        commands = {
+            "EOF": "end of file",
+            "help": "help command to show description of each command",
+            "quit": "Quit command to exit the program"
+        }
+
+        if arg:
+            print(f"{commands[arg]}\n")
+        else:
+            print("\n" + "Documented commands (type help <topic>):")
+            print("========================================")
+            for command in commands.keys():
+                print(f"{command}", end="  ")
+            print("\n")
+
+    def emptyline(self):
+        """
+        Handles empty line + ENTER to not print anything
+        """
+        return False
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     HBBNCommand().cmdloop()
