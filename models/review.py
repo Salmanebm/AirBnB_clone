e3d3414c9a2d8a7cf9e5f0a96837c0238c83582e
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-11 07:09:23 +0200

    Adding review classes

--- models/review.py
+++ models/review.py
@@ -5,4 +5,10 @@ This module has the Review class
 from models.base_model import BaseModel
 
 
-
+class Review(BaseModel):
+    """
+    Represents Review class
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
