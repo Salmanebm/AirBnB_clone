e6b086b086a8936d8f38c404acf7090a2904e396
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-12 19:48:35 +0200

    Adding update() method to the console.py

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ import json
 import sys
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
